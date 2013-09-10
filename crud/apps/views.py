3bd7946ff055e8dc998cdc004be53accd4a7e429
Herley Puerta <hypuerta@hotmail.com>
2013-09-09 19:31:11 -0500

    validando formularios de usuario

--- crud/apps/views.py
+++ crud/apps/views.py
@@ -9,50 +9,6 @@ def index_view(request):
 	lista_logro = Logro.objects.filter(activo=True)
 	return render_to_response('index.html', locals(),context_instance=RequestContext(request))
 
-<<<<<<< HEAD
-"""
-=======
-
-def del_asignatura_view(request, id_asignatura):
-	try:
-		Logro.objects.get(idAsignatura = id_asignatura)
-		asignatura = Asignatura.objects.get(idAsignatura = id_asignatura)
-		asignatura.delete()
-	except Exception, e:
-		pass
-	
-	return HttpResponseRedirect("/")
-
-
-def del_logro_view(request, id_logro):
-	logro = Asignatura.objects.get(idLogro = id_logro)
-	logro.delete()
-	return HttpResponseRedirect("/")
-
->>>>>>> 13cb55bb7771d762986fb39ff2a6c40eeaead4b7
-def add_asignatura_view(request):
-	if request.method == 'POST':
-		info = "inicializando"
-		form = addAsignaturaForm(request.POST)
-		if form.is_valid():
-			nombre = form.cleaned_data['nombre']
-			pesoAsignatura = form.cleaned_data['pesoAsignatura']
-			asignatura = Asignatura()
-			asignatura.nombre=nombre
-			asignatura.pesoAsignatura=pesoAsignatura
-			asignatura.activo=True
-			asignatura.save()
-			info = "datos guardados"
-		else:
-			info="informacion con datos incorrectos"
-		form = addAsignaturaForm()
-		return HttpResponseRedirect('/')
-	else:
-		form = addAsignaturaForm()
-		ctx={'form':form}
-		return render_to_response('addasignatura.html',ctx,context_instance=RequestContext(request))
-"""
-
 def add_asignatura_view(request):
 	if request.method == 'POST':
 		nombre = request.POST['nombre']
--- crud/urls.py
+++ crud/urls.py
@@ -7,10 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^',include('crud.apps.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$','index_view',name='vista_Principal'),
-    url(r'^del_asignatura/(?P<id_asignatura>.*)', 'del_asignatura_view'),
-    url(r'^del_logro/(?P<id_logro>.*)', 'del_logro_view'),
-    url(r'^addasignatura/$','add_asignatura_view',name='vista_nueva_asignatura'),
-    url(r'^addlogro/$','add_logro_view',name='vista_nuevo_logro'),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}),
 )
